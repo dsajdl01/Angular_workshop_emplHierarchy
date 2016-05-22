795768a524c757ad057269a3ab51dbc12293e22b
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-05-22 17:57:26 +0100

    midified AuthServices.js by adding permission.

--- HierarchyProject/js/service/AuthServices.js
+++ HierarchyProject/js/service/AuthServices.js
@@ -1,24 +1,22 @@
 
-myMngtHierarchyApp.factory('Auth', [ 'commonNodeHeirarchyModel', function(commonNodeHeirarchyModel) {
-	return new Auth(commonNodeHeirarchyModel);
+myMngtHierarchyApp.factory('Auth', [ 'commonNodeHeirarchyModel', 'mngtHierarchyNodeServiceProvider',
+ function(commonNodeHeirarchyModel, mngtHierarchyNodeServiceProvider) {
+	return new Auth(commonNodeHeirarchyModel, mngtHierarchyNodeServiceProvider);
 }]);
 // http://www.stefanoscerra.it/permission-based-auth-system-in-angularjs/
-function Auth(commonNodeHeirarchyModel)
+function Auth(commonNodeHeirarchyModel, mngtHierarchyNodeServiceProvider)
 {
 
 	var self = this;
 
-	self.checkIfAssumedIdentity = function(view){
-		if(!view.requireAssumedIdentity){
+	self.checkIfAssumedIdentity = function(view)
+    {
+		if(!view.requireAssumedIdentity)
+        {
 			return true;
 		}
-		return isUserAssumedIdentity();
-	}    
-     
-    var isUserAssumedIdentity = function(){
-    	console.log(commonNodeHeirarchyModel.isUserAssumeIdentity);
-    	return commonNodeHeirarchyModel.isUserAssumeIdentity;
-    }
+        return isUserAssumedIdentity();
+	}
 
     self.checkPermissionForView = function(view)
     {
@@ -26,20 +24,17 @@ function Auth(commonNodeHeirarchyModel)
         {
             return true;
         }
-        return userHasPermissionForView(view);
-    };
-     
-     
-    var userHasPermissionForView = function(view){
-        if(!view.permissions || !view.permissions.length)
-        {
-            return true;
-        } 
-        return self.userHasPermission(view.permissions);
+
+        var result = userHasPermissionForView(view);
+
+        if(!view.requirePersonalDataEntry){
+            return result;
+        }
+        return hasEnterPeronalDate(view);
     };
      
     self.userHasPermission = function(permissions)
-    {     
+    {
         var found = false;
         angular.forEach(permissions, function(permission, index)
         {
@@ -47,8 +42,60 @@ function Auth(commonNodeHeirarchyModel)
             {
                 found = true;
                 return;
-            }                        
+            }
         });
         return found;
     };
+
+    var userHasPermissionForView = function(view)
+    {
+        if(!view.permissions || !view.permissions.length)
+        {
+            return true;
+        }
+        return self.userHasPermission(view.permissions);
+    };
+
+    var hasEnterPeronalDate = function(view)
+    {
+        if(!view.personalDataEntry || !view.personalDataEntry.length)
+        {
+            return true;
+        }
+        return isPersonalDataEntered(view.personalDataEntry);
+    }
+
+    var isPersonalDataEntered = function(personalData)
+    {
+        var details = getDetails();
+        var result = false;
+        angular.forEach(personalData, function(data, index) {
+            if(!getDetailsValue(data, details)){
+                result = true;
+            }
+        });
+        return result;
+    }
+
+    var getDetailsValue = function(data, details){
+        switch(data)
+        {
+            case 'email':
+                return details.email;
+            case 'dob':
+                return details.dob;
+            default:
+                return null;
+        }
+    }
+
+    var getDetails = function()
+    {
+        return mngtHierarchyNodeServiceProvider.getSelectedNodeDetails(commonNodeHeirarchyModel.userSelectedNode.id);
+    }
+
+    var isUserAssumedIdentity = function()
+    {
+        return commonNodeHeirarchyModel.isUserAssumeIdentity;
+    }
 }
