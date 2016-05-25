52616f95054c171f39a3fd210fa98f33dbf2376b
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-05-25 20:54:48 +0100

    modified myProfileContrller.js by adding instance variables for class.

--- HierarchyProject/js/controller/myProfileContrller.js
+++ HierarchyProject/js/controller/myProfileContrller.js
@@ -1,12 +1,32 @@
 myMngtHierarchyApp.controller('myProfileContrller',['calculateTimeService', 'commonNodeHeirarchyModel', 'mngtHierarchyNodeServiceProvider',
 				 function(calculateTimeService, commonNodeHeirarchyModel, mngtHierarchyNodeServiceProvider) {
-	
+
 	var self = this;
+
 	self.showInputForms = false;
-	self.btnName = "Save";
+
+	self.init = function(){
+		self.btnName = "Save";
+		var emplDetails = mngtHierarchyNodeServiceProvider.getSelectedNodeDetails(commonNodeHeirarchyModel.userSelectedNode.id);
+		self.employeeName = commonNodeHeirarchyModel.userSelectedNode.name;
+		self.Access = commonNodeHeirarchyModel.userSelectedNode.access;
+		self.dob = emplDetails.dob;
+		self.email = emplDetails.email;
+		self.position = emplDetails.possition;
+		self.startDay = emplDetails.start;
+		self.employeeFullName = emplDetails.fullname;
+		self.hasAccess = doesUserHasAccess();
+		self.oldPasswoord = null;
+		self.newPassword = null;
+		self.confirmPassword = null;
+		setOriginalValues();
+	}
 
 	self.showPasswordFields =function(){
 		self.showInputForms = (self.showInputForms) ? false : true;
+		self.oldPasswoord = null;
+		self.newPassword = null;
+		self.confirmPassword = null;
 	}
 
 	self.makeChange = function(){
@@ -16,6 +36,19 @@ myMngtHierarchyApp.controller('myProfileContrller',['calculateTimeService', 'com
 	self.save = function(){
 		commonNodeHeirarchyModel.hasPersonalData = false;
 		console.log("self.commonNodeHeirarchyModel.hasPersonalData: ", commonNodeHeirarchyModel.hasPersonalData);
+	}
+
+	var setOriginalValues = function(){
+		self.originalEmplName = self.employeeName;
+		self.originalDob = self.dob;
+		self.originalEmail = self.email;
+		self.originalPosition = self.position
+		self.originalStartDate = self.startDay;
+		self.originalEmplFullName = self.employeeFullName;
+	}
+
+	var doesUserHasAccess = function(){
+		return !(commonNodeHeirarchyModel.userSelectedNode.access == "admin" || commonNodeHeirarchyModel.userSelectedNode.access == "viewer");
 	}
 
 }]);
