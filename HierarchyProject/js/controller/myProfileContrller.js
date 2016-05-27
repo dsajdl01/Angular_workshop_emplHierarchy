6a2127ac8a0b951092463c8f33c031683dfc812f
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-05-27 11:51:45 +0100

    modified myProfileContrller.js file by adding first logick for my profile view.

--- HierarchyProject/js/controller/myProfileContrller.js
+++ HierarchyProject/js/controller/myProfileContrller.js
@@ -1,3 +1,6 @@
+/**
+* Created by david on 15/04/16.
+*/
 myMngtHierarchyApp.controller('myProfileContrller',['calculateTimeService', 'commonNodeHeirarchyModel', 'mngtHierarchyNodeServiceProvider',
 				 function(calculateTimeService, commonNodeHeirarchyModel, mngtHierarchyNodeServiceProvider) {
 
@@ -7,7 +10,7 @@ myMngtHierarchyApp.controller('myProfileContrller',['calculateTimeService', 'com
 
 	self.init = function(){
 		self.btnName = "Save";
-		var emplDetails = mngtHierarchyNodeServiceProvider.getSelectedNodeDetails(commonNodeHeirarchyModel.userSelectedNode.id);
+		var emplDetails = getEmlpDetails();
 		self.employeeName = commonNodeHeirarchyModel.userSelectedNode.name;
 		self.Access = commonNodeHeirarchyModel.userSelectedNode.access;
 		self.dob = emplDetails.dob;
@@ -15,40 +18,157 @@ myMngtHierarchyApp.controller('myProfileContrller',['calculateTimeService', 'com
 		self.position = emplDetails.possition;
 		self.startDay = emplDetails.start;
 		self.employeeFullName = emplDetails.fullname;
-		self.hasAccess = doesUserHasAccess();
-		self.oldPasswoord = null;
+		self.hasAccess = doesUserHasAccessToModified();
+		self.oldPassword = null;
 		self.newPassword = null;
 		self.confirmPassword = null;
+		self.btnEqualValue = true;
 		setOriginalValues();
 	}
 
 	self.showPasswordFields =function(){
 		self.showInputForms = (self.showInputForms) ? false : true;
-		self.oldPasswoord = null;
+		self.oldPassword = null;
 		self.newPassword = null;
 		self.confirmPassword = null;
+		self.makeChange();
+		setMessagesToNull();
 	}
 
 	self.makeChange = function(){
-		
+		self.btnEqualValue = equal();
+		self.btnName = (self.btnEqualValue)? "Done": "Save";
 	}
 
 	self.save = function(){
-		commonNodeHeirarchyModel.hasPersonalData = false;
-		console.log("self.commonNodeHeirarchyModel.hasPersonalData: ", commonNodeHeirarchyModel.hasPersonalData);
+		console.log("hjgh isPaswordValid: ", isPaswordValid());
+		setMessagesToNull();
+		if(isPaswordValid()){
+			console.log("hjgh oldPasswoord:", !self.oldPassword);
+			if(self.oldPassword){
+				if(oldPasswordMatch()){
+					console.log("hjgh");
+					savePassword()
+					saveUserInputDate();
+					self.showInputForms = self.showPasswordFields();
+				} else {
+					self.old_passwordMsg = "Incorrect Current Password";
+				}
+			} else {
+				saveUserInputDate();
+			}
+		} else {
+			displayPasswordMessage();
+		}
+		console.log("self.commonNodeHeirarchyModel.hasPersonalData: ", commonNodeHeirarchyModel.hasPersonalData, "equal: ", equal());
 	}
+	var setMessagesToNull = function(){
+		self.old_passwordMsg = null;
+		self.confirm_passwordMsg = null;
+		self.new_passwordMsg = null;
+	}
+
+	var displayPasswordMessage = function()
+    {
+        if(!self.confirmPassword)
+        {
+            if(!self.confirmPassword && !self.oldPassword)
+            {
+                self.old_passwordMsg = "Current password is required.";
+                self.confirm_passwordMsg = true;
+            }
+            else if (!self.confirmPassword && !self.newPassword)
+            {
+                self.new_passwordMsg = true;
+                self.confirm_passwordMsg = true;
+            }
+            else
+            {
+                self.confirm_passwordMsg = true;
+            }
+        }
+        else if (!self.newPassword)
+        {
+            if(!self.newPassword && !self.oldPassword)
+            {
+                self.new_passwordMsg = true;
+                self.old_passwordMsg = "Current password is required.";
+            }
+            else
+            {
+                self.new_passwordMsg = true;
+            }
+        }
+        else {
+            self.old_passwordMsg = "Current password is required.";
+        }
+    }
+
+	var savePassword = function(){
+		var emplDetails = getEmlpDetails();
+		emplDetails.password = self.newPassword;
+	}
+
+	var saveUserInputDate = function(){
+		var emplDetails = getEmlpDetails();
+		emplDetails.email =  self.email;
+		emplDetails.possition = self.position;
+		emplDetails.dob = self.dob;
+		emplDetails.start = self.startDay;
+		emplDetails.fullname = self.employeeFullName;
+		commonNodeHeirarchyModel.userSelectedNode.name = self.employeeName;
+		commonNodeHeirarchyModel.hasPersonalData = false;
+		self.btnName = "Done";
+		self.btnEqualValue = true;
+	}
+
+	var oldPasswordMatch = function(){
+		var emplDetails = getEmlpDetails();
+		return self.oldPassword == emplDetails.password
+	}
+
+	var isPaswordValid = function(){
+		console.log("con: ",self.confirmPassword, " .. old: ",self.oldPassword, " ..new: ", self.newPassword);
+        if(!self.confirmPassword && !self.oldPassword && !self.newPassword)
+        {
+            return true;
+        }
+        if(self.confirmPassword && self.oldPassword && self.newPassword)
+        {
+            return true;
+        }
+        return false;
+    }
 
 	var setOriginalValues = function(){
 		self.originalEmplName = self.employeeName;
 		self.originalDob = self.dob;
 		self.originalEmail = self.email;
-		self.originalPosition = self.position
+		self.originalPosition = self.position;
 		self.originalStartDate = self.startDay;
 		self.originalEmplFullName = self.employeeFullName;
+		self.originalOldPassword = self.oldPassword;
+		self.originalNewPassword = self.newPassword;
+		self.originalConfirmPassword = self.confirmPassword;
 	}
 
-	var doesUserHasAccess = function(){
+	var equal = function(){
+		return self.originalEmplName == self.employeeName
+				&& self.originalDob == self.dob
+				&& self.originalEmail == self.email
+				&& self.originalPosition == self.position
+				&& self.originalStartDate == self.startDay
+				&& self.originalEmplFullName == self.employeeFullName
+				&& self.originalOldPassword == self.oldPassword
+				&& self.originalNewPassword == self.newPassword
+				&& self.originalConfirmPassword == self.confirmPassword;
+	}
+
+	var doesUserHasAccessToModified = function(){
 		return !(commonNodeHeirarchyModel.userSelectedNode.access == "admin" || commonNodeHeirarchyModel.userSelectedNode.access == "viewer");
 	}
 
+	var getEmlpDetails = function(){
+		return mngtHierarchyNodeServiceProvider.getSelectedNodeDetails(commonNodeHeirarchyModel.userSelectedNode.id);
+	}
 }]);
