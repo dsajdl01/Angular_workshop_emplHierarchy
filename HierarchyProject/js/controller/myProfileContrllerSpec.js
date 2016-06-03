e2d8ca8e9665b7b4c2a87f3b2cd99d9b23668b7a
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-06-03 21:46:01 +0100

    modified myProfileContrllerSpec.js by adding the rest of the test for myProfile Controller

--- HierarchyProject/js/controller/myProfileContrllerSpec.js
+++ HierarchyProject/js/controller/myProfileContrllerSpec.js
@@ -18,18 +18,19 @@ describe('Controller: myProfileContrller', function() {
 	{   
 		commonNodeHeirarchyModelMock = {
 			userSelectedNode: selectedNode[0]
-			// commonNodeHeirarchyModel.userSelectedNode
 		};
 
 		mngtHierarchyProviderMock = {
-			getSelectedNodeDetails: function(id){
+			getSelectedNodeDetails: function(id) {
 				if(id == 101) return details[0];
 				else if (id == 102) return details[1];
 				else return null;
 			}
 		};
 
-		toasterMock = {};
+		toasterMock = {
+			pop: function(error,message, option) {}
+		};
 
 		$provide.value('commonNodeHeirarchyModel', commonNodeHeirarchyModelMock);
 		$provide.value('mngtHierarchyNodeServiceProvider', mngtHierarchyProviderMock);
@@ -46,7 +47,8 @@ describe('Controller: myProfileContrller', function() {
         expect(ctrl).toBeDefined();
     });
 
-    it('should initialize instance variables when init() function is called', function(){
+    it('should initialize instance variables when init() function is called', function()
+    {
     	ctrl.init();
 
     	expect(ctrl.showInputForms).toBeFalsy();
@@ -64,11 +66,11 @@ describe('Controller: myProfileContrller', function() {
 		expect(ctrl.confirmPassword).toBeNull();
 		expect(ctrl.btnEqualValue).toBeTruthy();
     	
-
 		assertEqualOriginalValues();
     });
 
-    it('should initialize instance variables when init() function is called - II', function(){
+    it('should initialize instance variables when init() function is called - II', function()
+    {
     	commonNodeHeirarchyModelMock.userSelectedNode = selectedNode[0].child[0];
 
     	ctrl.init();
@@ -91,7 +93,8 @@ describe('Controller: myProfileContrller', function() {
 		assertEqualOriginalValues_II();
     });
 
-    it('should open and close pa fields when showPasswordFields is called', function(){
+    it('should open and close pa fields when showPasswordFields is called', function()
+    {
     	ctrl.init();
     	expect(ctrl.showInputForms).toBeFalsy();
     	// user open password's fields
@@ -109,7 +112,8 @@ describe('Controller: myProfileContrller', function() {
 		expect(ctrl.confirmPassword).toBeNull();
     });
 
-    it('should disable and rename buton when values are not same as original', function(){
+    it('should disable and rename buton when values are not same as original', function()
+    {
     	ctrl.init();
     	// button should be disable and name Save
     	expect(ctrl.btnName).toEqual("Save");
@@ -129,10 +133,182 @@ describe('Controller: myProfileContrller', function() {
     	ctrl.makeChange();
     	expect(ctrl.btnName).toEqual("Save");
     	expect(ctrl.btnEqualValue).toBeFalsy();
+    });
 
-    })
+    it('should save personal data and called toaster when all data passed  and save function is called', function()
+    {
+    	ctrl.init();
+    	ctrl.employeeName = "Bobik";
+    	ctrl.employeeFullName = "Bobik Freeman";
 
-	var assertEqualOriginalValues = function(){
+    	expect(ctrl.originalEmplName).toEqual("Bob");
+    	expect(ctrl.originalEmplFullName).toEqual("Bob Freeman");
+    	spyOn(toasterMock, 'pop');
+
+    	ctrl.save();
+
+    	expect(ctrl.originalEmplName).toEqual("Bobik");
+    	expect(ctrl.originalEmplFullName).toEqual("Bobik Freeman");
+    	expect(toasterMock.pop).toHaveBeenCalledWith("success","Done", "The data was successfully saved.");
+
+    	ctrl.employeeName = "Bob";
+    	ctrl.employeeFullName = "Bob Freeman";
+
+    	ctrl.save();
+    	expect(ctrl.originalEmplName).toEqual("Bob");
+    	expect(ctrl.originalEmplFullName).toEqual("Bob Freeman");
+    	expect(toasterMock.pop).toHaveBeenCalledWith("success","Done", "The data was successfully saved.");
+    });
+
+    it('should display error  message when user try to save and enter only new password', function()
+    {
+    	ctrl.init();
+    	ctrl.showPasswordFields();
+
+    	expect(ctrl.old_passwordMsg).toBeNull();
+		expect(ctrl.confirm_passwordMsg).toBeNull();
+		expect(ctrl.new_passwordMsg).toBeNull();
+
+    	ctrl.newPassword = "newPassword";
+    	ctrl.save();
+
+    	expect(ctrl.old_passwordMsg).toEqual("Current password is required.");
+        expect(ctrl.confirm_passwordMsg).toBeTruthy();
+        expect(ctrl.new_passwordMsg).toBeNull();
+    });
+
+     it('should display error  message when user try to save and enter only old password', function()
+     {
+    	ctrl.init();
+    	ctrl.showPasswordFields();
+
+    	expect(ctrl.old_passwordMsg).toBeNull();
+		expect(ctrl.confirm_passwordMsg).toBeNull();
+		expect(ctrl.new_passwordMsg).toBeNull();
+
+    	ctrl.oldPassword = "1234";
+
+    	ctrl.save();
+    	expect(ctrl.old_passwordMsg).toBeNull();
+        expect(ctrl.confirm_passwordMsg).toBeTruthy();
+        expect(ctrl.new_passwordMsg).toBeTruthy();
+    });
+
+     it('should display error  message when user try to save and enter only confirm password', function()
+     {
+    	ctrl.init();
+    	ctrl.showPasswordFields();
+
+    	expect(ctrl.old_passwordMsg).toBeNull();
+		expect(ctrl.confirm_passwordMsg).toBeNull();
+		expect(ctrl.new_passwordMsg).toBeNull();
+
+    	ctrl.confirmPassword = "newPassword";
+
+    	ctrl.save();
+    	expect(ctrl.old_passwordMsg).toEqual("Current password is required.");
+        expect(ctrl.confirm_passwordMsg).toBeNull();
+        expect(ctrl.new_passwordMsg).toBeTruthy();
+    });
+
+    it('should display error  message when user try to save and enter old password and confirm password', function()
+    {
+    	ctrl.init();
+    	ctrl.showPasswordFields();
+
+    	expect(ctrl.old_passwordMsg).toBeNull();
+		expect(ctrl.confirm_passwordMsg).toBeNull();
+		expect(ctrl.new_passwordMsg).toBeNull();
+
+    	ctrl.oldPassword = "1234";
+    	ctrl.confirmPassword = "newPassword";
+
+    	ctrl.save();
+    	expect(ctrl.old_passwordMsg).toBeNull();
+        expect(ctrl.confirm_passwordMsg).toBeNull();
+        expect(ctrl.new_passwordMsg).toBeTruthy();
+    });
+
+    it('should display error  message when user try to save and enter old password and new password', function()
+    {
+    	ctrl.init();
+    	ctrl.showPasswordFields();
+
+    	expect(ctrl.old_passwordMsg).toBeNull();
+		expect(ctrl.confirm_passwordMsg).toBeNull();
+		expect(ctrl.new_passwordMsg).toBeNull();
+
+    	ctrl.oldPassword = "1234";
+    	ctrl.newPassword = "newPassword";
+
+    	ctrl.save();
+    	expect(ctrl.old_passwordMsg).toBeNull();
+        expect(ctrl.confirm_passwordMsg).toBeTruthy();
+        expect(ctrl.new_passwordMsg).toBeNull();
+    });
+
+    it('should display error  message when user try to save and enter new password and confirm password', function()
+    {
+    	ctrl.init();
+    	ctrl.showPasswordFields();
+
+    	expect(ctrl.old_passwordMsg).toBeNull();
+		expect(ctrl.confirm_passwordMsg).toBeNull();
+		expect(ctrl.new_passwordMsg).toBeNull();
+
+    	ctrl.confirmPassword = "newPassword";
+    	ctrl.newPassword = "newPassword";
+
+    	ctrl.save();
+    	expect(ctrl.old_passwordMsg).toEqual("Current password is required.");
+        expect(ctrl.confirm_passwordMsg).toBeNull();
+        expect(ctrl.new_passwordMsg).toBeNull();
+    });
+
+    it('should display error  message when user try to save and enter incorrect old password', function()
+    {
+    	ctrl.init();
+    	ctrl.showPasswordFields();
+
+    	expect(ctrl.old_passwordMsg).toBeNull();
+		expect(ctrl.confirm_passwordMsg).toBeNull();
+		expect(ctrl.new_passwordMsg).toBeNull();
+
+    	ctrl.oldPassword = "whatEver";
+    	ctrl.newPassword = "newPassword";
+    	ctrl.confirmPassword = "newPassword";
+
+    	ctrl.save();
+    	expect(ctrl.old_passwordMsg).toEqual("Incorrect Current Password");
+        expect(ctrl.confirm_passwordMsg).toBeNull();
+        expect(ctrl.new_passwordMsg).toBeNull();
+    });
+
+    it('should save personal data and assgn passwords instance to null when save is called and all data is entered', function()
+    {
+    	ctrl.init();
+    	ctrl.showPasswordFields();
+
+    	expect(ctrl.old_passwordMsg).toBeNull();
+		expect(ctrl.confirm_passwordMsg).toBeNull();
+		expect(ctrl.new_passwordMsg).toBeNull();
+    	spyOn(toasterMock, 'pop');
+
+    	ctrl.oldPassword = "1234";
+    	ctrl.newPassword = "newPassword";
+    	ctrl.confirmPassword = "newPassword";
+
+    	ctrl.save();
+
+    	expect(ctrl.old_passwordMsg).toBeNull();
+        expect(ctrl.confirm_passwordMsg).toBeNull();
+        expect(ctrl.new_passwordMsg).toBeNull();
+        expect(toasterMock.pop).toHaveBeenCalledWith("success","Done", "The data was successfully saved.");
+        assertEqualOriginalValues();
+    });
+
+	var assertEqualOriginalValues = function()
+	{
 		expect(ctrl.originalEmplName).toEqual("Bob");
 		expect(ctrl.originalDob).toEqual("18/10/1973");
 		expect(ctrl.originalEmail).toEqual("bob.freeman@example.co.uk");
@@ -144,9 +320,10 @@ describe('Controller: myProfileContrller', function() {
 		expect(ctrl.originalConfirmPassword).toBeNull();
 	};
 
-	var assertEqualOriginalValues_II = function(){
+	var assertEqualOriginalValues_II = function()
+	{
 		expect(ctrl.originalEmplName).toEqual("Adam");
-		expect(ctrl.originalDob).toBeNull()
+		expect(ctrl.originalDob).toBeNull();
 		expect(ctrl.originalEmail).toBeNull();
 		expect(ctrl.originalPosition).toEqual("Designer");
 		expect(ctrl.originalStartDate).toEqual("21/02/2008");
