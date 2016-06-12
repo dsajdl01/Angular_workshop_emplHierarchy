d2f5c29ee6540d5b15a823f88ea171adafbbc60c
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-06-12 17:00:15 +0100

    modified hierarchyNodeServiceSpec.js file by adding test for getLoginDetails() function

--- HierarchyProject/js/service/hierarchyNodeServiceSpec.js
+++ HierarchyProject/js/service/hierarchyNodeServiceSpec.js
@@ -2,6 +2,7 @@ describe('Service: hierarchyNodeService', function() {
 
 	var service, httpBackend;
 	var contextRoot = 'https://raw.githubusercontent.com/dsajdl01/Angular_workshop_emplHierarchy/master/';
+    var contextRootBranch = 'https://raw.githubusercontent.com/dsajdl01/Angular_workshop_emplHierarchy/logIn.branch/';
 
 	var nodes = [{"name": "Sandra","id": 100,"parentsId": null,"child": [
 				{"name":"Bob","id": 101,"parentsId": 100,"child": []}]}];
@@ -9,6 +10,8 @@ describe('Service: hierarchyNodeService', function() {
 		{"id": 100, "dob": "28/02/1976","start": "09/12/2001","possition": "Senior Developer","comments": "Work with smile in our company."},
 		{"id": 101, "dob": "28/02/1981","start": "05/02/2005","possition": "Junior Developer","comments": "Work with hard for our company."}];
 				
+    var login = [{"nodeId": 101, "username": "bob", "password": "1234567890", "administrator": false }];
+
 	beforeEach(module('myMngtHierarchyApp'));
 
 	beforeEach(inject(function($injector, _$httpBackend_) {
@@ -29,7 +32,7 @@ describe('Service: hierarchyNodeService', function() {
         var callbackCalled = false;
 
         var callback = function(responce) {
-        	expect(responce).toEqual(nodes);
+          	expect(responce).toEqual(nodes);
         	callbackCalled = true; 
         };
 
@@ -86,6 +89,40 @@ describe('Service: hierarchyNodeService', function() {
         };
 
         service.getNodesDetails(function(){}, failCallback);
+        httpBackend.flush();
+        expect(callbackCalled).toBeTruthy();
+    });
+
+    it('should load user details when getLoginDetails is call', function()
+    {
+        httpBackend.whenGET(contextRootBranch + 'usersProfileLogin.json').respond(200, login);
+        httpBackend.expectGET(contextRootBranch + 'usersProfileLogin.json');
+
+        var callbackCalled = false;
+
+        var callback = function(responce) {
+            expect(responce).toEqual(login);
+            callbackCalled = true;
+        };
+
+        service.getLoginDetails(callback, function(){});
+        httpBackend.flush();
+        expect(callbackCalled).toBeTruthy();
+    });
+
+    it('should call back failure function when getLoginDetails returns an error code', function()
+    {
+        httpBackend.whenGET(contextRootBranch + 'usersProfileLogin.json').respond(500, "Internal error");
+        httpBackend.expectGET(contextRootBranch + 'usersProfileLogin.json');
+
+        var callbackCalled = false;
+
+        var failCallback = function(responce) {
+            expect(responce.data).toEqual("Internal error");
+            callbackCalled = true;
+        };
+
+        service.getLoginDetails(function(){}, failCallback);
         httpBackend.flush();
         expect(callbackCalled).toBeTruthy();
     });
