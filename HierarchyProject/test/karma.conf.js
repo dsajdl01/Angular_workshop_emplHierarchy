410cbd2b51d77812fc2a7fec159c12b331351d00
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-06-13 20:59:51 +0100

    modified karma.conf.js file by adding coverage to view report cogerage on test

--- HierarchyProject/test/karma.conf.js
+++ HierarchyProject/test/karma.conf.js
@@ -1,7 +1,5 @@
-// Karma test runner configuration
-
-// BSW - To run these tests manually (mithout Maven), from the prject root (e.g. emport-web) run:
-//      ./node_modules/karma/bin/karma start src/test/javascript/karma.conf.js
+// Karma configuration
+// Generated on Sun Jun 12 2016 21:15:03 GMT+0100 (BST)
 
 module.exports = function(config) {
   config.set({
@@ -9,46 +7,49 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '../js/',
 
+
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
+
     // list of files / patterns to load in the browser
     files: [
-      'vendor/angular/angular.js',
-      'vendor/angularjs-toaster/toaster.js',
-      'vendor/angular-animate/angular-animate.js',
-      'vendor/angular-mocks/angular-mocks.js',
-      'vendor/angular-route/angular-route.js',
-      'vendor/angular-resource/angular-resource.js',
-      'vendor/angular-messages/angular-messages.js',
-      'vendor/angular-sanitize/angular-sanitize.js',
-      'vendor/angular-strap/dist/angular-strap.js',
-      'vendor/angular-strap/dist/angular-strap.tpl.js',
-      'vendor/bower_components/angular-ui-validate/dist/validate.js',
-      'vendor/angular-ui-utils/ui-utils.js',
-      'vendor/angular-ui-select/dist/select.js',
+	'vendor/angular/angular.js',
+	'vendor/angularjs-toaster/toaster.js',
+        'vendor/angular-animate/angular-animate.js',
+        'vendor/angular-mocks/angular-mocks.js',
+        'vendor/angular-route/angular-route.js',
+        'vendor/angular-resource/angular-resource.js',
+        'vendor/angular-messages/angular-messages.js',
+        'vendor/angular-sanitize/angular-sanitize.js',
+        'vendor/angular-strap/dist/angular-strap.js',
+        'vendor/angular-strap/dist/angular-strap.tpl.js',
+        'vendor/bower_components/angular-ui-validate/dist/validate.js',
+        'vendor/angular-ui-utils/ui-utils.js',
+        'vendor/angular-ui-select/dist/select.js',
 
-      'app.js',
-      'controller/*.js',
-      'directives/*.js',
-      'service/*.js'
+        'app.js',
+        'controller/*.js',
+        'directives/*.js',
+        'service/*.js'
+
     ],
 
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: [
-        'progress'
-    ],
 
     // list of files to exclude
     exclude: [
     ],
 
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      "js/views/accounts.html": ["ng-html2js"]
+      ' app.j': ['coverage'],
+        'controller/*.js': ['coverage'],
+        'directives/*.js': ['coverage'] ,
+        'service/*.js': ['coverage'],
+      	"js/views/accounts.html": [""]
     },
 
     ngHtml2JsPreprocessor: {
@@ -56,30 +57,52 @@ module.exports = function(config) {
         // use stripPrefix and prependPrefix to adjust it.
         stripPrefix: "HierarchyProject/js/views/.*/",
         prependPrefix: "HierarchyProject/js/views/",
-
+ 
         // the name of the Angular module to create
         moduleName: "myMngtHierarchyApp"
     },
 
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter: {
+      type : 'html',
+      dir : '../coverage/'
+    },
+
+
     // web server port
-    port: 8080,
+    port: 9876,
+
 
     // enable / disable colors in the output (reporters and logs)
     colors: true,
+
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
+
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
+
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
+
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  });
-};
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
