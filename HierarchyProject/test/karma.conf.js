b7898e81165c803a146df6b01962f17e00c08cbe
dsajdl01 <davidsajdl@yahoo.co.uk>
2016-06-16 21:48:09 +0100

    added karma.conf.ci.js and modified karma.conf.js file by spliting karma function on testing and coverage

--- HierarchyProject/test/karma.conf.js
+++ HierarchyProject/test/karma.conf.js
@@ -41,17 +41,6 @@ module.exports = function(config) {
     exclude: [
     ],
 
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      ' app.j': ['coverage'],
-        'controller/*.js': ['coverage'],
-        'directives/*.js': ['coverage'] ,
-        'service/*.js': ['coverage'],
-      	"js/views/accounts.html": [""]
-    },
-
     ngHtml2JsPreprocessor: {
         // If your build process changes the path to your templates,
         // use stripPrefix and prependPrefix to adjust it.
@@ -67,13 +56,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
-
-    coverageReporter: {
-      type : 'html',
-      dir : '../coverage/'
-    },
-
+    reporters: ['progress'],
 
     // web server port
     port: 9876,
@@ -99,7 +82,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+    singleRun: false,
 
     // Concurrency level
     // how many browser should be started simultaneous
