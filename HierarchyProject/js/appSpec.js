696e7841555fa12ea16aef32f56372dc9607bc2b
David Sajdl <davidsajdl@yahoo.co.uk>
2016-06-26 22:14:43 +0100

    modified appSpec.js file by adding the rest of the tests for app

--- HierarchyProject/js/appSpec.js
+++ HierarchyProject/js/appSpec.js
@@ -1,11 +1,12 @@
 'use strict';
+
 describe('myMngtHierarchyApp: module', function() {
 
 	var $rootScope, locationMock, authMock;
 
   	beforeEach(module('myMngtHierarchyApp'));
 
-     beforeEach(module(function($provide) {
+    beforeEach(module(function($provide) {
         locationMock = {
 			path: function(path) {}
         };
@@ -13,11 +14,11 @@ describe('myMngtHierarchyApp: module', function() {
         authMock = {
         	hasPermission: true,
         	assumedIdentity: true, 
-        	checkIfAssumedIdentity: function(next){
+        	checkIfAssumedIdentity: function(next) {
         		if(this.assumedIdentity) return this.assumedIdentity;
         		else return this.assumedIdentity;
         	},
-        	checkPermissionForView: function(next){
+        	checkPermissionForView: function(next) {
         		if(this.hasPermission) return this.hasPermission;
         		else return this.hasPermission;
         	}
@@ -27,25 +28,18 @@ describe('myMngtHierarchyApp: module', function() {
         $provide.value('Auth', authMock);
     }));
 
-    beforeEach(inject(function(_$rootScope_, $templateCache) {
+    beforeEach(inject(function(_$rootScope_)
+    {
             $rootScope = _$rootScope_;
-
-            // We need add the template entry into the templateCache if we ever
-            // specify a templateUrl
-            $templateCache.put('js/views/selectedNodeInfo.html', '');
-            $templateCache.put('js/views/accountManagementHierarchyInfo.html', '');
-            $templateCache.put('js/views/AssumeIdentityTemplate.html', '');
-            $templateCache.put('js/views/myProfile.html', '');
-            $templateCache.put('js/views/personalInfoForm.html', '');
     }));
 
-    it('should be defined', function() {
+    it('should be defined', function()
+    {
         expect(myMngtHierarchyApp).toBeDefined();
     });
 
-
-    it('should test routesgo load particular ,html page view', inject(function ($route) {
-
+    it('should test routes and load particular or html page view', inject(function ($route)
+    {
 		expect($route.routes['/homeViewAccount'].templateUrl).toEqual('js/views/selectedNodeInfo.html');
 
 		expect($route.routes['/viewAccountInfo'].templateUrl).toEqual('js/views/accountManagementHierarchyInfo.html');
@@ -57,15 +51,34 @@ describe('myMngtHierarchyApp: module', function() {
 		expect($route.routes['/personalInfo'].templateUrl).toEqual('js/views/personalInfoForm.html');
 
 		expect($route.routes[null].redirectTo).toEqual('/homeViewAccount');
-
 	}));
 
-	it('should redirect to template Assume Identity when user does not assumed identity', function() {
-		authMock.assumedIdentity = false,
+	it('should redirect to template Assume Identity when user does not assumed identity', function()
+	{
+		authMock.assumedIdentity = false;
 		spyOn(locationMock, 'path');
 
-		$rootScope.$broadcast("$routeChangeStart", "dds");
+		$rootScope.$broadcast("$routeChangeStart", "next");
 		expect(locationMock.path).toHaveBeenCalledWith("/templateAssumeIdentity");
 	});
 
+	it('should redirect to homeViewAccount view when user assumed identity and does not has permission', function()
+	{
+		authMock.assumedIdentity = true;
+		authMock.hasPermission = false;
+		spyOn(locationMock, 'path');
+
+		$rootScope.$broadcast("$routeChangeStart", "next");
+		expect(locationMock.path).toHaveBeenCalledWith("/homeViewAccount");
+	});
+
+	it('should redirect to particular view if user assumed identity and has permission', function()
+	{
+		authMock.assumedIdentity = true;
+		authMock.hasPermission = true;
+		spyOn(locationMock, 'path');
+
+		$rootScope.$broadcast("$routeChangeStart", "next");
+		expect(locationMock.path).not.toHaveBeenCalled();
+	});
 });
